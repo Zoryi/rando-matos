d9b5b286b85684c17ecdaf06982b72512622c199
Romain Devalle <31740752+Zoryi@users.noreply.github.com>
2025-06-04 18:54:51 +0200

    Delete hiking_gear_manager directory

--- hiking_gear_manager/lib/models.dart
+++
@@ -1,6 +0,0 @@
-export 'models/category.dart';
-export 'models/material.dart';
-export 'models/kit.dart';
-export 'models/expedition.dart';
-export 'models/kit_material_relation.dart';
-export 'models/expedition_kit_relation.dart';
